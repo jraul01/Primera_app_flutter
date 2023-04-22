652be3eeafd28146e8ef06da3b683a8ccd47b4e6
jraul01 <raularcos67@gmail.com>
2023-04-22 02:25:27 -0500

    primera subida

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_application_1/presentacion/screens/counter/counter_functions_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+} 
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue
+      ),
+      home: const CounterFunctionsScreen()
+    );
+  }
+
+}
